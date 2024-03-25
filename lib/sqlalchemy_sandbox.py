d0490508ca8040e1094c600e2e04c58720abb95e
Leyian <victor.leyian@student.moringaschool.com>
2024-03-25 13:18:47 +0300

    done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -5,8 +5,28 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+# !data models/models == classes
 class Student(Base):
-    pass
+    # !name of sql database table
+    __tablename__ = 'students'
+    
+    # !attributes/fields
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    
 
 if __name__ == '__main__':
-    pass
+    '''
+    Refactoring any scripted elements of our modules to fit into this block makes sure that the script only runs when we tell it to.
+    '''
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
+
+'''
+The create_all() command on the next line tells the engine that any models that were created using Base as a parent should be used to create tables.
+'''
+
+'''
+chmod +x lib/sqlalchemy_sandbox.py
+python3 lib/sqlalchemy_sandbox.py
+'''
